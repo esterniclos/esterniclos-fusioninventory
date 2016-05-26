9e6a077704cd4f4aac45b1d3369dd03ef649f88a
Ester Niclós Ferreras <ester.niclos@catastro.minhap.es>
2016-05-26 12:19:15 +0200

    Cpan use for rhel >= 7

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,7 +1,23 @@
 class fusioninventory::install
 inherits fusioninventory::params
 {
+
+
+  # Rhel 7 has to be installed as cpan
+  # Cpan should be configured
+  if (( $::osfamily != "RedHat" ) and ($::os::release::major == '7')) {
+    include cpan
+    cpan { "Clone::Closure":
+      ensure  => present,
+      require => Class['FusionInventory::Agent'],
+      force   => true,
+    }
+    
+    }
+  else {
+    # Debian and ubuntu
     package {  $fusioninventory::params::pkgfusion:
         ensure => true,
     }
+  }
 }
