21228274868e0fc89392a57a019ead330dfb8bd0
Ester Niclós Ferreras <ester.niclos@catastro.minhap.es>
2016-05-27 07:51:58 +0200

    minor change: " to \'

--- manifests/install.pp
+++ manifests/install.pp
@@ -5,19 +5,21 @@ inherits fusioninventory::params
 
   # Rhel 7 has to be installed as cpan
   # Cpan should be configured
-  if (( $::osfamily != "RedHat" ) and ($::os::release::major == '7')) {
-    include cpan
-    cpan { "Clone::Closure":
-      ensure  => present,
-      require => Class['FusionInventory::Agent'],
-      force   => true,
+  if (( $::osfamily == 'RedHat' ) and ($::os::release::major == '7')) {
+      include cpan
+      cpan { "Clone::Closure":
+        ensure  => present,
+        require => Class['FusionInventory::Agent'],
+        force   => true,
+      }
     }
-    
-    }
+
   else {
     # Debian and ubuntu nad rhel lower than 7
     package {  $fusioninventory::params::pkgfusion:
-        ensure => true,
+      ensure => true,
     }
   }
 }
+
+
